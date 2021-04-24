d5f21523ce50a8c297b512b89eaef88fb8c31100
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2021-04-25 00:12:05 +0530

    Adding subscribing to mono with Consumer

--- src/test/java/com/rahullokurte/reactive/test/MonoTest.java
+++ src/test/java/com/rahullokurte/reactive/test/MonoTest.java
@@ -2,6 +2,7 @@ package com.rahullokurte.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -12,12 +13,37 @@ public class MonoTest {
     void monoSubscriber() {
         String name = "Rahul Lokurte";
         Mono<String> mono = Mono.just(name).log();
-
         mono.subscribe();
         log.info("---------------");
 
         StepVerifier.create(mono)
                 .expectNext(name)
                 .verifyComplete();
+    }
+
+    @Test
+    void monoSubscriberConsumer() {
+        String name = "Rahul Lokurte";
+        Mono<String> mono = Mono.just(name).log();
+        mono.subscribe(s -> log.info("name: {}",s));
+        log.info("---------------");
+
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
+    }
+
+    @Test
+    void monoSubscriberConsumerError() {
+        String name = "Rahul Lokurte";
+        Mono<String> mono = Mono.just(name)
+                .map(s -> {throw new RuntimeException("Testing some error");});
+        mono.subscribe(s -> log.info("name: {}",s), s-> log.error("Something bad happened" ));
+        mono.subscribe(s -> log.info("name: {}",s), Throwable::printStackTrace);
+        log.info("---------------");
+
+        StepVerifier.create(mono)
+                .expectError(RuntimeException.class)
+                .verify();
     }
 }
