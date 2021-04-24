4ea6b45048903fa0de7d08f767fb49ef24b0ff5e
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2021-04-24 23:38:37 +0530

    ProjectSetUp: Adding dependencies

---
+++ src/test/java/com/rahullokurte/reactive/test/MonoTest.java
@@ -0,0 +1,13 @@
+package com.rahullokurte.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    void test() {
+        log.info("Everything working as expected");
+    }
+}
