f5ab8031f83182cfb6717894be44866fd433d796
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2021-04-24 23:53:48 +0530

    Subscribing to Mono

--- src/test/java/com/rahullokurte/reactive/test/MonoTest.java
+++ src/test/java/com/rahullokurte/reactive/test/MonoTest.java
@@ -2,12 +2,22 @@ package com.rahullokurte.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
 
     @Test
-    void test() {
-        log.info("Everything working as expected");
+    void monoSubscriber() {
+        String name = "Rahul Lokurte";
+        Mono<String> mono = Mono.just(name).log();
+
+        mono.subscribe();
+        log.info("---------------");
+
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
     }
 }
